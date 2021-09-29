15be01b23cebbc187b4c926780e729a95feadac3
Alessandro Venditti <alessandro.venditti1995@gmail.com>
2021-09-29 18:48:24 +0200

    Added param to default endpoint

--- src/main/java/io/rrredbeard/kube_hello_app/KubeHelloApplication.java
+++ src/main/java/io/rrredbeard/kube_hello_app/KubeHelloApplication.java
@@ -1,5 +1,8 @@
 package io.rrredbeard.kube_hello_app;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.rrredbeard.kube_hello_app.config.KubeHelloAppProperties;
 import io.rrredbeard.kube_hello_app.dto.AppInfo;
 import io.rrredbeard.kube_hello_app.utils.PrintableBean;
@@ -11,6 +14,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -20,43 +24,43 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @Slf4j
 @SpringBootApplication
 @EnableWebMvc
-@EnableConfigurationProperties({ KubeHelloAppProperties.class })
+@EnableConfigurationProperties({KubeHelloAppProperties.class})
 public class KubeHelloApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KubeHelloApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(KubeHelloApplication.class, args);
+  }
 
-	public static void logConfig(@NonNull PrintableBean bean) {
-		if(log.isDebugEnabled()){
-			log.debug("LOADED CONF :: {}", bean);
-		}
-	}
+  public static void logConfig(@NonNull PrintableBean bean) {
+    if (log.isDebugEnabled()) {
+      log.debug("LOADED CONF :: {}", bean);
+    }
+  }
 
+  /* ***/
 
-	/* ***/
+  @RestController
+  @RequestMapping(produces = APPLICATION_JSON_VALUE)
+  @RequiredArgsConstructor
+  public static class Controller {
 
+    private final KubeHelloAppProperties properties;
+    private final ObjectMapper objectMapper;
 
-	@RestController
-	@RequestMapping(produces = APPLICATION_JSON_VALUE)
-	@RequiredArgsConstructor
-	public static class Controller {
+    @NonNull
+    @GetMapping("/{param}")
+    public ResponseEntity<AppInfo> getInfo(@NonNull @PathVariable("param") String param) {
 
-		private final KubeHelloAppProperties properties;
+      final ObjectNode params = objectMapper.createObjectNode();
 
-		@NonNull
-		@GetMapping
-		public ResponseEntity<AppInfo> getInfo(){
+      params.put("path_variable", param);
 
-			return ResponseEntity.ok(
-				AppInfo.builder()
-					.appId(properties.getId())
-					.version(properties.getVersion())
-					.build()
-			);
-		}
-
-	}
-
-
+      return ResponseEntity.ok(
+          AppInfo.builder()
+              .appId(properties.getId())
+              .version(properties.getVersion())
+              .extraParams(params)
+              .build());
+    }
+  }
 }
--- src/main/java/io/rrredbeard/kube_hello_app/dto/AppInfo.java
+++ src/main/java/io/rrredbeard/kube_hello_app/dto/AppInfo.java
@@ -2,6 +2,7 @@ package io.rrredbeard.kube_hello_app.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,5 +18,8 @@ public class AppInfo {
 
 	@JsonProperty()
 	private String version;
+
+	@JsonProperty(value = "extra_params")
+	private JsonNode extraParams;
 
 }
